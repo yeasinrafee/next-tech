75af35a1d7f163224ccc6e957636898b6354a0df
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-09-07 21:29:47 +0600

    environment variables

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,7 +6,9 @@ export const metadata = {
 };
 
 const getTime = async () => {
-  const res = await fetch('http://localhost:3000/time', { cache: 'no-store' });
+  const res = await fetch(`http://localhost:3000/time`, {
+    cache: 'no-store',
+  });
   const data = await res.json();
 
   return data.currentTime;
